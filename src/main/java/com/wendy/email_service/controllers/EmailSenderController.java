2a7cb1dc3ba1ae47a932fb6947a63ad5ea730bdf
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2024-06-10 23:08:14 -0300

    created controller and exception class

---
+++ src/main/java/com/wendy/email_service/controllers/EmailSenderController.java
@@ -0,0 +1,34 @@
+package com.wendy.email_service.controllers;
+
+import com.wendy.email_service.application.EmailSenderService;
+import com.wendy.email_service.core.EmailRequest;
+import com.wendy.email_service.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
+        try {
+            emailSenderService.sendEmail(emailRequest.to(), emailRequest.subject(), emailRequest.body());
+            return ResponseEntity.ok("Email sent successfully!");
+        } catch (EmailServiceException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Email sending failed.");
+        }
+    }
+}
